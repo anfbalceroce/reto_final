0d672b373f654bbc08d87c5c0f95d1a148eb32ce
anfbalceroce <anfbalceroce@unal.edu.co>
2023-03-21 07:53:51 -0500

    tests setup

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -20,7 +20,11 @@ import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-ad
 | Feel free to remove existing plugins or add more.
 |
 */
-export const plugins: Required<Config>['plugins'] = [assert(), runFailedTests(), apiClient()]
+export const plugins: Required<Config>['plugins'] = [
+  assert(),
+  runFailedTests(),
+  apiClient()
+]
 
 /*
 |--------------------------------------------------------------------------
@@ -47,7 +51,11 @@ export const reporters: Required<Config>['reporters'] = [specReporter()]
 |
 */
 export const runnerHooks: Pick<Required<Config>, 'setup' | 'teardown'> = {
-  setup: [() => TestUtils.ace().loadCommands()],
+  setup: [
+    () => TestUtils.ace().loadCommands(),
+    () => TestUtils.db().migrate(),
+    () => TestUtils.db().seed()
+  ],
   teardown: [],
 }
 

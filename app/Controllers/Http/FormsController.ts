71ab58e6dad31301fd55de7136631c1765818a94
anfbalceroce <anfbalceroce@unal.edu.co>
2023-03-21 04:10:46 -0500

    added form control finished requested crud

--- app/Controllers/Http/FormsController.ts
+++ app/Controllers/Http/FormsController.ts
@@ -1,17 +1,47 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Answer from 'App/Models/Answer';
+import Form from 'App/Models/Form';
+import Question from 'App/Models/Question';
+import User from 'App/Models/User';
 
 export default class FormsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ response }: HttpContextContract) {
+    try {
+      response.status(200).json({"state": true,
+                                "questions": await (await Question.query().where('state', true).preload('answers'))
+                                .map((question: Question) => {
+                                  return {                                    
+                                    question: question.question,
+                                    id: question.id,
+                                    options: question.answers.map((answer: Answer) => {
+                                      return {
+                                        id: answer.id,
+                                        option: answer.answer
+                                      }
+                                    })
+                                  }
+                                })
+                                });
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({"state": false, "message": "Error al obtener el listado"});
+    }
+  }
 
-  public async create({}: HttpContextContract) {}
-
-  public async store({}: HttpContextContract) {}
-
-  public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
+  public async create({ request, response }: HttpContextContract) {
+    try {
+      const { estudianteId, answers } = request.all();
+      await User.findOrFail(estudianteId);
+      for (const answer of answers) {
+        await Answer.findOrFail(answer);
+      }
+      answers.forEach(async (answer: number) => {
+        await Form.create({studentId: estudianteId, answerId: answer, state: true});
+      });
+      response.status(201).json({"state": true, "message": "Respuestas almacenadas con exito"});
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({"state": false, "message": "No se pudieron almacenar las respuestas"});
+    }
+  }
 }
